3e7eb82ae2269d057b88e5cc9ae571136c309118
harishpate <harishpatel6648@gmail.com>
2023-08-19 16:42:37 +0530

    sixth variable

---
+++ 06-variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Please enter your name"
+
+read USERNAME
+
+echo "Username entered is: $USENAME"
+
+echo "Please enter your password"
+read PASSWORD
+echo "Password entered is: $PASSWORD"
