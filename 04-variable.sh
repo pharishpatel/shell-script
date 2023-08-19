33e6cef8411196f7ff1869a4bcd3ca33f26db475
harishpate <harishpatel6648@gmail.com>
2023-08-19 16:26:43 +0530

    fourth variable

---
+++ 04-variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+PERSON1=$1
+PERSON2=$2
+
+
+echo "$PERSON1: hi $PERSON2, good morning"
+echo "$PERSON2: good morning $PERSON1"
+echo "$PERSON1: how are you $PERSON2"
+echo "$PERSON2: i am good, how are you"
