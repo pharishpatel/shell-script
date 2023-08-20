e073e6cf8c20c188d6084fcfadca904930071f49
harishpate <harishpatel6648@gmail.com>
2023-08-20 13:53:59 +0530

    conditions

--- 10-validate.sh
+++ 10-validate.sh
@@ -16,6 +16,13 @@ else
 fi
 }
 
+if [ $USERID -ne 0 ]
+then 
+    echo "ERROR:: please run this script with root access"
+    exit 1
+# else
+#   echo "INFO:: you are root user"
+fi
 
 # it is our responsibility to check installation succes or not
 yum install mysql -y
